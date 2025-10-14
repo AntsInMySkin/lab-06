89c2d47d9d0fe655461e759accc44f29e7e868c4
AntsInMySkin <leventozkan2003@gmail.com>
2025-10-14 17:18:32 -0600

    Before unit testing, getCities() throws an error for some reason

--- code/app/src/main/java/com/example/code/City.java
+++ code/app/src/main/java/com/example/code/City.java
@@ -1,29 +1,38 @@
 package com.example.code;
 
+/**
+ * City class that defines the city and province variables, along with the getter and setter methods
+ * for each variable.
+ */
 public class City {
-    private String name;
+    private String city;
     private String province;
 
-    public City(String name, String province) {
-        this.name = name;
+    public City(String city, String province) {
+        this.city = city;
         this.province = province;
     }
 
-    public String getName() {
-        return name;
+    public String getCityName() {
+        return this.city;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCityName(String city) {
+        this.city = city;
     }
 
-    public String getProvince() {
-        return province;
+    public String getProvinceName() {
+        return this.province;
     }
 
-    public void setProvince(String province) {
+    public void setProvinceName(String province) {
         this.province = province;
     }
 
+    public int compareTo(Object o) {
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName()); // this.city refers to the city name
+    }
 }
+
 
