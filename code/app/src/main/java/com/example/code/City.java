b95a7a03fb6ca3b02a9141842504ae9a33afd48d
AntsInMySkin <leventozkan2003@gmail.com>
2025-10-14 18:05:04 -0600

    Some unit testing implemented

--- code/app/src/main/java/com/example/code/City.java
+++ code/app/src/main/java/com/example/code/City.java
@@ -30,6 +30,10 @@ public class City implements Comparable<City> {
     }
 
     @Override
+    /**
+     * This method compares cities based on city name field
+     * @return -1 if the caller is greater than the city name field of called object
+     */
     public int compareTo(City city) {
         return this.name.compareTo(city.getName());
     }
